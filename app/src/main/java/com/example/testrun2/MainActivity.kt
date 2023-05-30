fba51f61f348a8104279e0b59dfb0c81698eedc4
PlutoTuer <jonasbeier67@gmail.com>
2023-05-30 20:26:23 +0200

    Bugs: Textfield isnt fully rendered //Bad formatting //Maybe disable input to be multiple lines

--- app/src/main/java/com/example/testrun2/MainActivity.kt
+++ app/src/main/java/com/example/testrun2/MainActivity.kt
@@ -39,7 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-
+//TODO: DEVELOPMENT BRANCH
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
